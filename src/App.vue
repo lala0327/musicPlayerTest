63aea28468e4705abb634874d56fad62231cd499
葉雅瑄 <lala@program.com.tw>
2022-04-20 09:53:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <ReloadPWA />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import ReloadPWA from "./components/ReloadPWA.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+    ReloadPWA,
+  },
+});
+</script>
---
+++ vite.config.js
@@ -0,0 +1,57 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const base = command === 'build' ? '/pwa-vite/' : '/';
+
+  return {
+    base: base,
+    plugins: [
+      vue(),
+      VitePWA({
+        mode: "development",
+        base: base,
+        srcDir: "src",
+        filename: "sw.js",
+        includeAssets: ["/favicon.ico"],
+        strategies: "injectManifest",
+        manifest: {
+          name: "PWA_Vite_Tailwind",
+          short_name: "PWA模板",
+          theme_color: "#ffffff",
+          start_url: base,
+          display: "standalone",
+          background_color: "#ffffff",
+          icons: [
+            {
+              "src": "./icon/manifest-icon-192.maskable.png",
+              "sizes": "192x192",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "./icon/manifest-icon-192.maskable.png",
+              "sizes": "192x192",
+              "type": "image/png",
+              "purpose": "maskable"
+            },
+            {
+              "src": "./icon/manifest-icon-512.maskable.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "./icon/manifest-icon-512.maskable.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "maskable"
+            }
+          ],
+        },
+      }),
+    ],
+  }
+})
